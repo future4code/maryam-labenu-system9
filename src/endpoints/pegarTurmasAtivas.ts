7fd23ff8d37bfd02d1759882fff21c5988c0b9b2
gabrielmina118 <gabrielmina118@gmail.com>
2021-12-14 15:48:44 -0300

    PR correção

--- src/endpoints/pegarTurmasAtivas.ts
+++ src/endpoints/pegarTurmasAtivas.ts
@@ -6,7 +6,7 @@ export default async function pegarTurmasAtivas (req: Request, res: Response){
         const resultado: any = await connection.raw(`
             SELECT * FROM Turma
             WHERE modulo > 0
-        `)
+        `);
         res.status(200).send(resultado[0])
     } catch (error: any) {
         res.status(400).send({message: error.message})
